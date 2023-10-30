ba660b1b5eb416724d52d58dc4e8e719a1bf1623
teddygavi <matc.davis@gmail.com>
2023-10-29 22:52:35 -0600

    more struct work!

--- src/header.ts
+++ src/header.ts
@@ -1,6 +1,5 @@
 /*
 The header contains the following fields:
-
                                     1  1  1  1  1  1
       0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
@@ -16,40 +15,12 @@ The header contains the following fields:
     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
     |                    ARCOUNT                    |
     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+*/
 
-where:
-
-ID              A 16 bit identifier assigned by the program that
-                generates any kind of query.  This identifier is copied
-                the corresponding reply and can be used by the requester
-                to match up replies to outstanding queries.
-
-QR              A one bit field that specifies whether this message is a
-                query (0), or a response (1).
-
-OPCODE          A four bit field that specifies kind of query in this
-                message.  This value is set by the originator of a query
-                and copied into the response.  The values are:
-
-                0               a standard query (QUERY)
-
-                1               an inverse query (IQUERY)
-
-                2               a server status request (STATUS)
-
-                3-15            reserved for future use
-
-AA              Authoritative Answer - this bit is valid in responses,
-                and specifies that the responding name server is an
-                authority for the domain name in question section.
-
-                Note that the contents of the answer section may have
-                multiple owner names because of aliases.  The AA bit
-
-  */
 export default class Header implements IHeader {
   id: number
   qr: number
+  tc: number
   constructor(id, qr) {
     this.id = id
     this.qr = qr
--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,59 @@
 interface IHeader {
   //Octet structure
   /**
-   * id is 16 bit unsigned (positive) integer
+   * @description id is 16 bit unsigned (positive) integer
    * @type {number}
    */
 
   id: number
 
   /**
-   *  A one bit field that specifies whether this message is a query (0), or a response (1).
-   *  @type {number}
+   * @description A one bit field that specifies whether this message is a query (0), or a response (1).
+   * @type {number}
    */
 
   qr: number
 
   /**
-   * A four bit field that specifies kind of query in this message.  This value is set by the originator of a query and copied into the response.  The values are:
+   * @description A four bit field that specifies kind of query in this message.  This value is set by the originator of a query and copied into the response.  The values are:
    * @type {number}
    */
 
   opcode: number
+
   /**
-   * Authoritative Answer - this bit is valid in responses and specifies that the responding name server is an authority for the domain name in question section.
+   * @description Authoritative Answer - this bit is valid in responses and specifies that the responding name server is an authority for the domain name in question section.
    * @type {number}
    */
 
   aa: number
 
+  /**
+   * @description TrunCation - specifies that this message was truncated due to length greater than that permitted on the transmission channel.
+   * @type {number}
+   */
+
   tc: number
+
+  /**
+   * @description Recursion Desired - this bit may be set in a query and is copied into the response.  If RD is set, it directs the name server to pursue the query recursive. Recursive query support is optional.
+   * @type {number}
+   */
+
   rd: number
+
+  /**
+   * @description Recursion Available - this be is set or cleared in a response, and denotes whether recursive query support is available in the name server.
+   * @type {number}
+   */
+
   ra: number
+
+  /**
+   * @description Reserved for future use.  Must be zero in all queries and responses
+   * @type {number}
+   */
+
   z: number
   rcode: number
   qdcount: number
